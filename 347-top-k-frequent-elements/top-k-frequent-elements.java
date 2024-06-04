716ee42763bdf7e3c94440fece7d765f147f9060
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-04 01:29:16 -0400

    Time: 13 ms (73.67%) | Memory: 48.7 MB (34.01%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,27 +1,27 @@
-class CustomNode {
-    int key;
-    int value;
-    public CustomNode(int key, int value){
-        this.key = key;
-        this.value = value;
-    }
-}
+// class CustomNode {
+//     int key;
+//     int value;
+//     public CustomNode(int key, int value){
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int arr[] = new int[k];
         HashMap<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<CustomNode> pq = new PriorityQueue<>((a, b) -> b.value - a.value);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         for(int num: nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         // System.out.println("Map" + map);
         // return new int[0];
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            pq.add(new CustomNode(entry.getKey(), entry.getValue()));
+            pq.add(entry);
         }
 
         for(int i=0; i<=k-1; i++){
-            arr[i] = pq.poll().key;
+            arr[i] = pq.poll().getKey();
 
         }
 
