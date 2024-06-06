37f708bfb44111aab9630bb700f17f22abd5e8ff
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-05 20:03:45 -0400

    Time: 15 ms (6.17%) | Memory: 44.5 MB (58.83%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
+         
+        List<Integer> list = new ArrayList<>();
         int index =0;
-        
-        List<Integer> currentList = new ArrayList<Integer>();
-        List<List<Integer>> resultList = new ArrayList<>();
-        findCombinationSum(index, candidates, target, currentList, resultList);
-        return resultList;   
+        List<List<Integer>> resultList= new ArrayList<>();
+        int currentSum =0;
+        findAllCombinationSum(candidates, target, list, currentSum, index, resultList);
+
+       
+
+         return resultList;
+         
     }
 
-    void findCombinationSum(int index, int[] candidates, int target, List<Integer> currentList, List<List<Integer>> resultList) {
+     void findAllCombinationSum(int[] candidates, int target, List<Integer> list, int currentSum,  int index, List<List<Integer>> resultList){
 
-        
-         if(index == candidates.length) {
-            //  System.out.println("CL " + currentList);
-             if(target== 0) {
-                 resultList.add(new ArrayList<>(currentList));
-             }
-             return;
+            if(index == candidates.length){ // index equlas candidate length
+                if(currentSum == target ){ // if currentSum == target it mean we got desired combination
+                    System.out.println("list " + list);
+                    resultList.add(new ArrayList<>(list));
+                }
+                return;
+            }
+            if(currentSum + candidates[index] <= target){ // tom kae currentsum doesnt exceed target
+                 list.add(candidates[index]);
+                 // calling the function for same index
+                 findAllCombinationSum(candidates, target, list, currentSum + candidates[index], index, resultList);
+                 if(list.size() >0){
+             list.remove(list.size()-1);
+           }
+            }
+            // if currentSum is greater than target move to next index without adding it
+            // findAllCombinationSum(candidates, target, list, currentSum, index, resultList);
+           
+            
+            findAllCombinationSum(candidates, target, list, currentSum, index+1, resultList);
+                       
          }
-         
-         if(target >= candidates[index]) {
-               currentList.add(candidates[index]);
-               findCombinationSum(index, candidates, target - candidates[index], currentList, resultList);
-               currentList.remove(currentList.size()-1);// jo add krunga vo hi remove hoga isliye add and remove written in same if block
-         }
-        
-        
-         findCombinationSum(index + 1, candidates, target, currentList, resultList );
-
-     }
 }
-// Lessons Learnt
-
-// follow pattern add remove vaala
-// to pick same element again and againbe on same index
-// condition when picking same element has to be stopped
-// target mai se minus krna hai and check if taarget == 0
-// Keeping add and remove in same if condition element which is addedd will only be removed
-
