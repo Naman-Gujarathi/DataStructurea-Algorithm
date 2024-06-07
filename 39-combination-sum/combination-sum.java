90da31b3abe80117013affaa3a5c6df301fed0ef
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-07 18:29:08 -0400

    Time: 2 ms (85.23%) | Memory: 44.4 MB (73.71%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -6,18 +6,15 @@ class Solution {
         List<List<Integer>> resultList= new ArrayList<>();
         int currentSum =0;
         findAllCombinationSum(candidates, target, list, currentSum, index, resultList);
-
-       
-
          return resultList;
          
     }
 
      void findAllCombinationSum(int[] candidates, int target, List<Integer> list, int currentSum,  int index, List<List<Integer>> resultList){
 
-            if(index == candidates.length){ // index equlas candidate length
+            if(index >= candidates.length){ // index equlas candidate length
                 if(currentSum == target ){ // if currentSum == target it mean we got desired combination
-                    System.out.println("list " + list);
+                    // System.out.println("list " + list);
                     resultList.add(new ArrayList<>(list));
                 }
                 return;
