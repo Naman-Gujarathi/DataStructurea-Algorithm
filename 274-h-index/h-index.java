a45c0b44b4203a358fe04810a1921e40edcaee8d
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-07 15:49:54 -0400

    Time: 2 ms (68.26%) | Memory: 41.4 MB (43.53%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,38 +1,46 @@
 class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations); //  1 2 3 4 5
-        int count =0;
-        boolean flag = false;
-        int hIndexValue = citations.length;// 5
-        // 0  1 2
-        // 4 -> 0
-        for(int i = citations.length -1; i>=0; i--){
-             if(count == hIndexValue){//  1 == 2
+        int hIndex =1;
+        for(int i=citations.length -1; i>=0; i-- ){
+            if(citations[i] >= hIndex){
+                hIndex++;
+            } else {
                 break;
             }
+        }
+        // int count =0;
+        // boolean flag = false;
+        // int hIndexValue = citations.length;// 5
+        // // 0  1 2
+        // // 4 -> 0
+        // for(int i = citations.length -1; i>=0; i--){
+        //      if(count == hIndexValue){//  1 == 2
+        //         break;
+        //     }
            
-            while(citations[i] < hIndexValue){ // 
-                hIndexValue--; // 
+        //     while(citations[i] < hIndexValue){ // 
+        //         hIndexValue--; // 
 
-                if(count == hIndexValue){//  1 == 2
-                   flag = true;
-                   break;
-                }
-            }
+        //         if(count == hIndexValue){//  1 == 2
+        //            flag = true;
+        //            break;
+        //         }
+        //     }
 
-             if(flag == true){
-                break;
-            }
+        //      if(flag == true){
+        //         break;
+        //     }
 
-            if(citations[i] >= hIndexValue){ // 5 5
-                count++; // 1
-            }
+        //     if(citations[i] >= hIndexValue){ // 5 5
+        //         count++; // 1
+        //     }
 
             
 
            
-        }
-
-        return hIndexValue;
+        // }
+        hIndex--;
+        return hIndex;
     }
 }
