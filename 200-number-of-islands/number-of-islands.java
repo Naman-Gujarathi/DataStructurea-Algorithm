281fa6bbcbeb8619e6c0f56938b676ec92905c6d
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-08 23:47:23 -0400

    Time: 4 ms (41.41%) | Memory: 49.4 MB (45.02%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+
+    int m = grid.length;
+    int n = grid[0].length;
+    boolean visitedNode[][] = new boolean[m][n];
+    int countIsland =0;
+    int nRow[] = {0, 1, 0, -1};
+    int nCol[] = {1, 0, -1, 0};
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if( grid[i][j] == '1' && !visitedNode[i][j]) {
+               
+                countIsland++;
+                findNumberIsland(visitedNode, grid, i, j, nRow, nCol, m, n);
+            }
+        }
+    }
+
+    return countIsland;
+
+    }
+
+    void findNumberIsland(boolean visitedNode[][], char[][] grid, int currRow, int currCol, int nRow[], int nCol[], int m, int n ){
+          visitedNode[currRow][currCol] = true;
+        for(int i=0; i<nRow.length; i++){
+            int adjRow = currRow + nRow[i];
+            int adjCol = currCol + nCol[i];
+
+            if(adjRow >= 0 && adjRow < m && adjCol >= 0 && adjCol < n && visitedNode[adjRow][adjCol] == false && grid[adjRow][adjCol] == '1' ){
+              
+                findNumberIsland(visitedNode, grid, adjRow, adjCol, nRow, nCol, m, n);
+            }
+        }
+
+    }
+}
