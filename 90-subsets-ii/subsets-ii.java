baea3ca529b316f21076e7639c5b5fbb37a5103c
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-09 20:12:54 -0400

    Time: 3 ms (27.15%) | Memory: 43.3 MB (52.59%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        Arrays.sort(nums);  // Sort the array to handle duplicates
-        findUniqueSubset(0, nums, new ArrayList<>(), resultList);
-        return resultList;
+        List<Integer> list = new ArrayList<>();
+        Set<List<Integer>> resultList = new HashSet<>();
+        int index = 0;
+        findUniqueSubset(index, nums, list, resultList);
+        return new ArrayList<>(resultList);
     }
 
-    void findUniqueSubset(int index, int[] nums, List<Integer> list, List<List<Integer>> resultList) {
-        resultList.add(new ArrayList<>(list));
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) continue; // Skip duplicates
-            list.add(nums[i]);
-            findUniqueSubset(i + 1, nums, list, resultList);
-            list.remove(list.size() - 1);
+    void findUniqueSubset(int index, int[] nums, List<Integer> list, Set<List<Integer>> resultList) {
+        if (index == nums.length) {
+            List<Integer> sortedList = new ArrayList<>(list);
+            Collections.sort(sortedList);
+            resultList.add(sortedList);
+            return;
         }
+
+        list.add(nums[index]);
+        findUniqueSubset(index + 1, nums, list, resultList);
+        list.remove(list.size() - 1);
+        findUniqueSubset(index + 1, nums, list, resultList);
     }
 }
