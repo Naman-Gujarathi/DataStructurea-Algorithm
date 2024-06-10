99f6928124048f16f769050a3382d54fe060ef74
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-10 19:42:19 -0400

    Time: 15 ms (88.17%) | Memory: 41.7 MB (98.38%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,26 +1,35 @@
+class Triplet {
+    int start;
+    int end;
+    int max;
+    public Triplet(){
+        this.start = 0;
+        this.end = 0;
+        this.max = 0;
+    }
+}
+
 class Solution {
     public String longestPalindrome(String s) {
         StringBuffer sb = new StringBuffer();
-        int start[] = {0};
-        int end[] = {0};
-        int max[] ={0};
+        Triplet obj = new Triplet();
           for(int i=0; i<s.length(); i++){
             int left =i;
             int right =i;
-            checkPalindrome(left, right, start, end, sb, s, max);
-            checkPalindrome(left, right+1, start, end, sb, s, max);
+            checkPalindrome(left, right,  sb, s, obj);
+            checkPalindrome(left, right+1, sb, s, obj);
             
         }
-        return s.substring(start[0], end[0] + 1);
+        return s.substring(obj.start, obj.end + 1);
     }
 
-    void checkPalindrome(int left, int right, int start[], int end[], StringBuffer sb, String s, int max[]){
+    void checkPalindrome(int left, int right,  StringBuffer sb, String s, Triplet obj) {
        
              while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                if(max[0] < (right - left + 1)){
-                    max[0] = right - left + 1;
-                    start[0] = left;
-                    end[0] = right;
+                if(obj.max < (right - left + 1)){
+                    obj.max = right - left + 1;
+                    obj.start = left;
+                    obj.end = right;
                 }
                 left--;
                 right++; 
