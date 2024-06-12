afd1f4fedb16933dd4f49cda5206f5984b1860f3
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-12 17:58:32 -0400

    Time: 4 ms (95.26%) | Memory: 44.4 MB (93.00%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,49 +1,72 @@
-class Pair {
-    int element;
-    int min;
-    public Pair(int element, int min){
-        this.element = element;
+class Node{
+     int value;
+     int min;
+     Node next;
+ 
+     public Node(int value, int min){
+        this.value = value;
         this.min = min;
+        this.next = null;
+     }
     }
-}
-
-
+ 
+ 
 class MinStack {
-    Stack<Pair> minstack;
-     Pair pair;
-     int smallest; 
-
+    Node head;
+    int currentMin;
     public MinStack() {
-        minstack = new Stack<>();
-        smallest = Integer.MAX_VALUE;
+       currentMin = Integer.MAX_VALUE;
+ 
         
     }
-    
     public void push(int val) {
+        currentMin = Math.min(val, currentMin);
+ 
+         Node temp = new Node(val, currentMin);
+         if(head == null){
+              head = temp;
+         } else {
+            temp.next = head;
+            head = temp;
+         }
 
-        smallest =  Math.min(val, smallest);
-        minstack.push(new Pair(val, smallest));
     }
-    
     public void pop() {
-        minstack.pop();
-        if(!minstack.isEmpty()){
-            smallest = minstack.peek().min;
-        } else{
-            smallest = Integer.MAX_VALUE;
+              if (head == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        head = head.next;
+        if (head != null) {
+            currentMin = head.min;
+        } else {
+            currentMin = Integer.MAX_VALUE;
         }
          
+    }
+    public int top() {
+             if (head == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return head.value;
         
     }
-    
-    public int top() {
-       return minstack.peek().element;
-    }
-    
     public int getMin() {
-       return minstack.peek().min; 
+              if (head == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return head.min;
+        
     }
 }
+ 
+/**
+* Your MinStack object will be instantiated and called as such:
+* MinStack obj = new MinStack();
+* obj.push(val);
+* obj.pop();
+* int param_3 = obj.top();
+* int param_4 = obj.getMin();
+*/
 
 /**
  * Your MinStack object will be instantiated and called as such:
