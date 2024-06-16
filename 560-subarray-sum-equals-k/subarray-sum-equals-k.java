7ed743319caea344a63329d8b73bc218b6dda327
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-16 15:12:34 -0400

    Time: 26 ms (38.47%) | Memory: 46.7 MB (25.08%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+         int currentPrefix =0; // currentPrefix sum 
+         int resultCount =0; // total resultCount
+         HashMap<Integer, Integer> map = new HashMap<>();
+         map.put(0, 1);
+         
+         for(int i=0; i<nums.length; i++){
+            currentPrefix = currentPrefix + nums[i];
+            if(map.containsKey(currentPrefix - k)){
+                resultCount = resultCount + map.get(currentPrefix - k);  
+            }
+            map.put(currentPrefix, map.getOrDefault(currentPrefix, 0) + 1);
+            // if(map.containsKey(currentPrefix)){
+            //     map.put(currentPrefix, map.getOrDefault(currentPrefix, 0) + 1);  
+            // }else{
+            //     map.put(currentPrefix, 1);
+            // }
+            //        
+         }
+
+         return resultCount;
+    }
+}
