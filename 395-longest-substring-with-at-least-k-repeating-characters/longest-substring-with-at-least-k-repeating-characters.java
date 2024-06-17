fd7a70b1a2f2574fea53547fc46d295966426a74
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-17 18:23:44 -0400

    Time: 1 ms (76.40%) | Memory: 41.2 MB (70.81%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -6,9 +6,16 @@ class Solution {
         if(k<=1){
             return s.length();
         }
-         HashMap<Character, Integer> map = new HashMap<>();
+
+        return findLongestSubString(s, k);
+    }
+
+    int findLongestSubString(String s, int k) {
+        if(s.length() < k){
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
         
-         
          for(int i=0; i<s.length(); i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
          }
@@ -25,7 +32,7 @@ class Solution {
         } 
 
        
-        int max1 = longestSubstring(s.substring(0, idx), k) ;
+        int max1 = findLongestSubString(s.substring(0, idx), k) ;
     
 
         while(idx < s.length() && map.get(s.charAt(idx)) < k){
@@ -33,12 +40,11 @@ class Solution {
         }
         int max2 =0;
         if(idx < s.length()-1 ){
-            max2 = longestSubstring(s.substring(idx, s.length()), k);
+            max2 = findLongestSubString(s.substring(idx, s.length()), k);
         }else{
             max2 =0;
         }
 
         return Math.max(max1, max2);
-
     }
 }
