dfe64c948372da193a61870a638362685c538632
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-18 17:48:14 -0400

    Time: 7 ms (58.81%) | Memory: 44.4 MB (85.82%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,44 +9,40 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> list1 = new ArrayList<>();
-        ArrayList<TreeNode> list2 = new ArrayList<>();
-        TreeNode lca_Value = null;
-         findLowestCommon(root, p,  list1);
-         findLowestCommon(root, q, list2);  
-
-         int n = Math.min(list1.size(), list2.size());
-        for(int i=0; i<n; i++){
-            if(list1.get(i).val == list2.get(i).val){
-                lca_Value = list1.get(i);
-                continue;
-            } else{
-               
-                break;
-            }
-       }
-         
-       return lca_Value;
+      return findLCA(root, p, q);
     }
 
-    boolean findLowestCommon(TreeNode node, TreeNode target, ArrayList<TreeNode> list) {
+    TreeNode findLCA(TreeNode node, TreeNode p, TreeNode q){
         if(node == null){
-            return false;
+            return null;
         }
 
-        list.add(node);
-        if(node.val == target.val){
-            return true;
+        if(node.val == p.val){
+            return p;
         }
-        if(findLowestCommon(node.left, target, list)){
-            return true;
+
+        if(node.val == q.val){
+            return q;
         }
-        if(findLowestCommon(node.right, target, list)){
-            return true;
+
+        TreeNode val1 = findLCA(node.left, p, q);
+        TreeNode val2 = findLCA(node.right, p, q);
+
+        if(val1 != null && val2 != null ){
+            return node;
         }
-        list.remove(list.size()-1);
-        return false;
+
+        else if(val1 == null && val2 == null ){
+            return null;
+        }
+
+        else if(val1 != null && val2 == null ){
+            return val1;
+        }
+
+        else {
+            return val2;
+        }
     }
+
 }
-// TC: O(3N) O(2N) for recursion call for both target p and target q as well for loop between list1 and list 2
-// SC: O(4N) for O(2N) call stack and O(2N) for list1 and list2
