65171e95bcbd6bc27fc4e1bc437829e5e786694c
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-20 14:40:54 -0400

    Time: 1362 ms (78.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select uni.unique_id, emp.name from Employees as emp left JOIN EmployeeUNI as uni on emp.id = uni.id; 
+
+
+-- emp.id   emp.name    uni.id   uni.unique_id
+-- 11        Meir.        11        2
+-- 90.       winston      90        3
+-- 3         jonathan     3         1
+-- 1           Alice.     null     null
+-- 7.         bob         null    null         
