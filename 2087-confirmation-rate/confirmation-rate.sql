1297ffc7d6468dce1efc6f8e8d0faa2ccc45e047
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-23 15:17:20 -0400

    Time: 590 ms (90.44%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,7 +2,7 @@
 
 With total_signups as (select s.user_id, count(*) as signup_count from Signups as s left join Confirmations as c  on s.user_id = c.user_id group by s.user_id),
 
-confirmed_signup as (select s.user_id, count(c.action) as confirmed_count from Signups as s left join Confirmations as c on s.user_id = c.user_id and c.action = 'confirmed'group by s.user_id)
+confirmed_signup as (select s.user_id, count(c.action) as confirmed_count from Signups as s left join Confirmations as c on s.user_id = c.user_id and c.action = 'confirmed' group by s.user_id )
 
 select t.user_id, Round((c.confirmed_count / t.signup_count), 2) as confirmation_rate from total_signups as t join confirmed_signup  as c
 on t.user_id = c.user_id;
