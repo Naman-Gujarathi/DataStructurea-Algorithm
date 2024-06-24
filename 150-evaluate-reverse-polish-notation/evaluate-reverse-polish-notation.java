1c6ac22ceecaceade2419f8f3fa3300c9ca8a7c2
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-24 19:03:09 -0400

    Time: 6 ms (70.27%) | Memory: 44.8 MB (24.31%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,45 +3,57 @@ class Solution {
         if(tokens.length == 1){
             return Integer.parseInt(tokens[0]);
         }
-        Stack<Integer> stack = new Stack<>(); // 3 3 *
-      
+        Stack<Integer> stack = new Stack<>(); 
         int result = 0;
         int val1 =0;
         int val2=0;
          for(int i=0; i<tokens.length; i++){
-        
-              if(tokens[i].equals("+")  || tokens[i].equals("-")  ||  tokens[i].equals("*") || tokens[i].equals("/")) {
-             
-                       val1 = stack.pop();
-                   if(!stack.isEmpty()){
-                       val2 = stack.pop();
+             switch(tokens[i]) {
+                case "+": 
+                   val1 = stack.pop();
+                   if(!stack.isEmpty()) {
+                     val2 = stack.pop();
+                   
                    }
-                  
-                   if(tokens[i].equals("+")){
-                        result = val1 + val2;
-                    
-                        stack.push(result); 
+                   
+                   result = val1 + val2;
+                   stack.push(result);
+                   break;
+
+                case "-": 
+                   val1 = stack.pop();
+                   if(!stack.isEmpty()) {
+                     val2 = stack.pop();
                    }
-                   else if(tokens[i].equals("-")){
-                      result =  val2 - val1;
-                       stack.push(result); 
+                   result =  val2 - val1;
+                   stack.push(result); 
+                    break;
+
+                case "*":
+                   val1 = stack.pop();
+                   if(!stack.isEmpty()) {
+                     val2 = stack.pop();
                    }
-                   else if(tokens[i].equals("*")){
-                         result =  val2 * val1;
-                          stack.push(result); 
-                   }else{
-                     if(val1 != 0){
-                         result = val2 / val1;
-                         stack.push(result);  
-                     }
-                    
+                   result =  val2 * val1;
+                   stack.push(result);
+                    break; 
+
+                case "/": 
+                   val1 = stack.pop();
+                   if(!stack.isEmpty()) {
+                     val2 = stack.pop();
                    }
-              } else {
-                int value = Integer.parseInt(tokens[i]);
-                stack.push(value); // 2 1
-                 
-              }
-         }
+                   result =  val2 / val1;
+                   stack.push(result);
+                   break; 
+
+                default: 
+                    int val = Integer.parseInt(tokens[i]);
+                    stack.push(val);
+                    break;    
+            }    
+        }
+
 
         return result;
     }
