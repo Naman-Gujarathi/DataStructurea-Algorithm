59f1ce1d6f3d5acd0ed20b84e2f3dcf9084c7cee
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-06-25 20:32:16 -0400

    Time: 52 ms (87.22%) | Memory: 68.9 MB (43.71%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,5 +1,9 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
+        // pepcoding
+        // Goal is to sort 2D array on first coloum ref and check with its next rows 0th row 
+        // and check if next row oth coloum value is less than equal to ref it mean it will 
+        // be burst using same arrow
         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
 
         // 1,6. 2,8. 7,12 10,16
