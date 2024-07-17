c6fbe659789f03e9902852f53466242ee4cd66da
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-07-17 17:22:37 -0400

    Time: 1 ms (99.97%) | Memory: 45.5 MB (63.54%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int result =0;
+        
+        int resultArray[] = new int[2];
+        resultArray[0] =0;
+        resultArray[1]=0;
+
+        for(int num : nums){
+            result = result ^ num;
+        }
+
+        int bit = 1;
+
+        while((bit & result) == 0 ) {
+            bit = bit << 1;
+        }
+
+        for(int num : nums){
+            if((bit & num) != 0){
+                resultArray[0] = resultArray[0] ^ num;
+            } else {
+                resultArray[1] = resultArray[1] ^ num;
+            }
+        }
+        
+        return resultArray;
+
+
+    }
+}
