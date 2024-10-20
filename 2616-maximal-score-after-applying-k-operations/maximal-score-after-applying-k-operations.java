fb4fd844743f7a681729056366f1a7a464d6c919
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-10-20 10:10:50 -0400

    Time: 122 ms (75.14%) | Memory: 63 MB (45.15%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.java
@@ -1,27 +1,27 @@
 class Solution {
     public long maxKelements(int[] nums, int k) {
         
-        PriorityQueue<Long> queue = new PriorityQueue<>((a,b) -> Long.compare(b,a));
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> b - a);
 
         long result = 0;
 
         for(int i=0; i<nums.length; i++){
             
-            queue.add((long)nums[i]);
+            queue.add(nums[i]);
         }
-        long temp = 0;
+        double temp = 0;
         long poppedValue = 0;
        for(int i=1; i<=k; i++){
         // System.out.println("queue.poll" + queue.poll());
-        poppedValue = (long) queue.poll();
+        poppedValue =  queue.poll();
         // System.out.println("poppedValue" + poppedValue);
         result = result + poppedValue;
         // System.out.println("result" + result);
 
-        temp = (long)Math.ceil((double) poppedValue/3);
+        temp = Math.ceil((double) poppedValue/3);
 
         // System.out.println(" temp " + temp);
-        queue.add(temp);
+        queue.add((int) temp);
 
         // System.out.println("queue current at " + i + "operation" + queue);
         //    result += queue.poll();
