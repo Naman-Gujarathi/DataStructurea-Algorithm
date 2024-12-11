57012fe8ae2439870c06e4fa6116f378541611ee
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-12-11 08:55:45 -0500

    Time: 142 ms (5.16%) | Memory: 46 MB (8.96%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+          StringBuffer sb = new StringBuffer();
+
+          if(s.trim().length() == 0) return true; // whitespaces 
+
+          for(int i=0; i<s.length(); i++){
+                if(((s.charAt(i) - '0') >= 0 && (s.charAt(i)) - '0' <= 9) || ((s.charAt(i) - '0') >= 17 && (s.charAt(i)) - '0' <= 42) || ((s.charAt(i) - '0') >= 49 && (s.charAt(i)) - '0' <= 74)){
+
+                    if(((s.charAt(i) - '0') >= 17 && (s.charAt(i)) - '0' <= 90) || ((s.charAt(i) - '0') >= 42)){
+                        sb.append(Character.toLowerCase(s.charAt(i)));
+                    }else{
+                        sb.append(s.charAt(i));
+                    }
+                    
+                }
+          } 
+           
+          
+
+          String alphaNumericStr = sb.toString();
+          char[] alphaNumericArr = alphaNumericStr.toCharArray();
+
+          int left = 0;
+          int right = alphaNumericArr.length - 1;
+
+        //   System.out.println("arr" + alphaNumericArr);
+
+          for(int i = 0; i<=right; i++){
+            System.out.print( alphaNumericArr[i]);
+          }
+
+          while(left <= right){
+            if(alphaNumericArr[left] != alphaNumericArr[right]){
+                return false;
+            }
+            left++;
+            right--;
+          }
+
+          return true;
+
+
+         
+
+        
+
+          
+    }
+}
+
