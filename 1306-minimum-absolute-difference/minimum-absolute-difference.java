416332ef4f90bd8446780d9b6739ca1957105cc1
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2024-12-13 19:37:51 -0500

    Time: 17 ms (93.41%) | Memory: 57.2 MB (16.80%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.java
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<List<Integer>> minimumAbsDifference(int[] arr) {
     Arrays.sort(arr);
-    HashSet<Integer> set = new HashSet<>();
-    set.add(arr[0]);
+    // HashSet<Integer> set = new HashSet<>();
+    // set.add(arr[0]);
     int min = Integer.MAX_VALUE;
         for(int i=0 ; i<arr.length-1; i++){
-             set.add(arr[i+1]);
+            //  set.add(arr[i+1]);
              int currValue = arr[i+1] - arr[i];
              min = Math.min(min, currValue);
         }
@@ -14,14 +14,16 @@ class Solution {
         // List<Integer> list = new ArrayList<>();
         List<List<Integer>> resultList = new ArrayList<>();
         
-        for(int i=0; i<arr.length; i++){
-            
-                if(set.contains(arr[i] + min)){
-                    // list.add(arr[i]);
-                    // list.add(arr[i] + min);
-                    // resultList.add(new ArrayList<>(list));
-                     resultList.add(Arrays.asList(arr[i], arr[i] + min));
-                } 
+        for(int i=0; i<arr.length -1; i++){
+                if((arr[i+1] - arr[i]) == min){
+                    resultList.add(Arrays.asList(arr[i], arr[i+1]));
+                }
+                // if(set.contains(arr[i] + min)){
+                //     // list.add(arr[i]);
+                //     // list.add(arr[i] + min);
+                //     // resultList.add(new ArrayList<>(list));
+                //      
+                // } 
             
             // list.clear();
         }
