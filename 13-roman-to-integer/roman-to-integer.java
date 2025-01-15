a0fb3f71cb04ed3231c8374757de8c3070d117b5
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2025-01-15 15:00:41 -0500

    Time: 3 ms (85.69%) | Memory: 44.7 MB (65.50%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,26 +1,41 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // map.put('I', 1);
+        // map.put('V', 5);
+        // map.put('X', 10);
+        // map.put('L', 50);
+        // map.put('C', 100);
+        // map.put('D', 500);
+        // map.put('M', 1000);
+
+
 
         int result = 0;
         for(int i=0; i<s.length()-1; i++){
-            if( map.get(s.charAt(i)) >=  map.get(s.charAt(i +1))){
-                result += map.get(s.charAt(i));
+            if( get(s.charAt(i)) >=  get(s.charAt(i +1))){
+                result += get(s.charAt(i));
             }else{
-                result -= map.get(s.charAt(i));
+                result -= get(s.charAt(i));
             }
         }
 
-        result = result + map.get(s.charAt(s.length()-1));
+        result = result + get(s.charAt(s.length()-1));
         return result;
 
+    }
+
+   int get(char character){
+        switch(character){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            default: return 1000;
+            
+        }
     }
 }
 
