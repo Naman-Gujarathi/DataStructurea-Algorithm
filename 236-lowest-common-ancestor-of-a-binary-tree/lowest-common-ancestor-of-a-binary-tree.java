1472193f780b80e18a450c7e886f9d90c463c85c
Naman Gujarathi <113138473+Naman-Gujarathi@users.noreply.github.com>
2025-01-28 14:33:56 -0500

    Time: 6 ms (100.00%) | Memory: 44.8 MB (47.76%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,40 +9,24 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-      return findLCA(root, p, q);
-    }
+        if(root == p || root == q || root == null){
+            return root;
+        }
+       
+        TreeNode temp1 = lowestCommonAncestor(root.left,p,q);
+       TreeNode temp2 = lowestCommonAncestor(root.right,p,q);
 
-    TreeNode findLCA(TreeNode node, TreeNode p, TreeNode q){
-        if(node == null){
+        if(temp1 != null && temp2  != null){
+            return root;
+        }
+        else if (temp1  != null && temp2  == null){
+             return temp1;
+        } else if (temp1  == null && temp2  != null){
+             return temp2 ;
+        }else{
             return null;
         }
 
-        if(node.val == p.val){
-            return p;
-        }
-
-        if(node.val == q.val){
-            return q;
-        }
-
-        TreeNode val1 = findLCA(node.left, p, q);
-        TreeNode val2 = findLCA(node.right, p, q);
-
-        if(val1 != null && val2 != null ){
-            return node;
-        }
-
-        else if(val1 == null && val2 == null ){
-            return null;
-        }
-
-        else if(val1 != null && val2 == null ){
-            return val1;
-        }
-
-        else {
-            return val2;
-        }
+       
     }
-
 }
